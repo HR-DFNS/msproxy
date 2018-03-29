67ce1de9d4dbab4cb218efd88cc7bd8590f04929
mabrsweeney <mabrsweeney@gmail.com>
2018-03-29 08:25:33 -0700

    redirects to load balancer

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const ReactDom = require('react-dom/server');
 const app = express();
 const port = process.env.PORT || 4000;
 const request = require('request');
+const redis = require('redis');
 const Layout = require('./templates/layout');
 const App = require('./templates/app');
 const Scripts = require('./templates/scripts');
@@ -29,26 +30,18 @@ app.get('/', (req, res) => {
   res.redirect('/restaurants/1/');
 });
 
-// app.use('/restaurants/:id', express.static(path.join(__dirname, 'public')));
 app.get('/restaurants/:id', function(req, res){
   let components = renderComponents(services);
-  res.end(Layout(
+  const rendered = Layout(
     'WeGot',
     App(...components),
     Scripts(Object.keys(services))
-  ));
+  );
+  res.end(rendered);
 });
+
 app.get('/api/restaurants/:id/gallery', (req, res) => {
-  res.redirect(`http://localhost:3001/api/restaurants/${req.params.id}/gallery`)
-});
-app.get('/api/restaurants/:id/overview', (req, res) => {
-  //res.redirect(`http://184.169.248.150/api/restaurants/${req.params.id}/overview`)
-});
-app.get('/api/restaurants/:id/sidebar', (req, res) => {
-  //res.redirect(`http://54.177.233.239/api/restaurants/${req.params.id}/sidebar`)
-});
-app.get('/api/restaurants/:id/recommendations', (req, res) => {
-  //res.redirect(`http://52.89.102.101/api/restaurants/${req.params.id}/recommendations`)
+  const response = res.redirect(`http://g-1938099559.us-west-1.elb.amazonaws.com/api/restaurants/${req.params.id}/gallery`);
 });
 
 app.listen(port, () => {
