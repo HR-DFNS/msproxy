c15c96a776045b9284d358e08f0cb4f945d83775
EC2 Default User <ec2-user@ip-172-31-8-155.us-west-1.compute.internal>
2018-03-30 21:29:43 +0000

    uses round robin style of load balancing; awslb in comments JIC

--- server.js
+++ server.js
@@ -18,32 +18,51 @@ const serviceConfig = require('./service-config.json');
 const services = require('./loader.js')(clientBundles, serverBundles, serviceConfig);
 
 const renderComponents = (components, props = {}) => {
-  return Object.keys(components).map(item => {
-    let component = React.createElement(components[item]);
-    return ReactDom.renderToString(component);
+	return Object.keys(components).map(item => {
+	  let component = React.createElement(components[item]);
+			return ReactDom.renderToString(component);
   });
 }
+
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.redirect('/restaurants/1/');
+	res.redirect('/restaurants/1/');
 });
 
 app.get('/restaurants/:id', function(req, res){
-  let components = renderComponents(services);
-  const rendered = Layout(
-    'WeGot',
-    App(...components),
-    Scripts(Object.keys(services))
-  );
-  res.end(rendered);
+	let components = renderComponents(services);
+	const rendered = Layout(
+		'WeGot',
+		App(...components),
+		Scripts(Object.keys(services))
+		);
+	res.end(rendered);
 });
-
+let service = 1;
 app.get('/api/restaurants/:id/gallery', (req, res) => {
-  const response = res.redirect(`http://g-1938099559.us-west-1.elb.amazonaws.com/api/restaurants/${req.params.id}/gallery`);
+	//*
+  switch(service) {
+		case 1:
+		  res.redirect(`http://54.153.68.14:3001/api/restaurants/${req.params.id}/gallery/`);
+		  console.log(1);
+      break;
+		case 2:
+		  res.redirect(`http://54.153.113.21:3001/api/restaurants/${req.params.id}/gallery/`);
+		  console.log(2);
+      break;
+		case 3:
+		  res.redirect(`http://54.153.16.167:3001/api/restaurants/${req.params.id}/gallery/`);
+		  console.log(3);
+      break;
+	}
+  service = service === 3 ? 1 : service+1;
+  /*/
+	res.redirect(`http://g-1938099559.us-west-1.elb.amazonaws.com/api/restaurants/${req.params.id}/gallery`); // */
 });
 
 app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`)
+	console.log(`server running at: http://localhost:${port}`)
 });
